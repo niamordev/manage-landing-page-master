dbc05181e26ea680f0a442e3c2521f62b4ebf465
NiamorDev <115006018+niamordev@users.noreply.github.com>
2022-11-09 14:33:09 +0100

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,13 @@
+let openNav = document.querySelector('.icon-hamburger')
+let closeNav = document.querySelector('.icon-close')
+
+let btnNavLinks = document.querySelector('.btn-nav-links')
+let navLinks = document.querySelector('.nav-links-mobile')
+let activeNavMobile = document.querySelector('.active-nav-mobile')
+
+
+btnNavLinks.addEventListener('click', function(){
+    navLinks.classList.toggle('active-nav-mobile')
+    openNav.classList.toggle('hidden')
+    closeNav.classList.toggle('hidden')
+})
